6a9d47c041512912a841128db051bea09916d3d0
tkarshi <tkarshi2000@gmail.com>
2024-07-15 05:43:48 +0530

    applying slice method for divide the text

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-let text = "welcome to javascript";
+let text = "Javascript";
 
 console.log(text);
+
+console.log(text.slice(4)); //take the 4th index element
+
+console.log(text.slice(0, 4)); //leave the 4th index element
