feec0a7fcf755079bb70474d0d45e82209324b2f
tkarshi <tkarshi2000@gmail.com>
2024-07-15 06:23:32 +0530

    repeat the text and count the length

--- digits.js
+++ digits.js
@@ -4,8 +4,16 @@
 
 // displayNumbers(5);
 
-let students = "student 1,student 2,student 3";
+// let students = "student 1,student 2,student 3";
 
-let Student = students.split(",");
+// let Student = students.split(",");
 
-console.log(Student);
+// console.log(Student);
+
+//REPEAT
+
+let text = "React ";
+
+console.log(text.repeat(3));
+
+console.log(text.length);
