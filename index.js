21803095f0b4e1d260c9d6c7c5b63f3c45127927
tkarshi <tkarshi2000@gmail.com>
2024-07-15 05:56:25 +0530

    using trim for remove the spaces in the text

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let text = "Javascript";
+// let text = "Javascript";
 
 // console.log(text);
 
@@ -7,8 +7,13 @@ let text = "Javascript";
 // console.log(text.slice(0, 4)); //leave the 4th index element
 
 //GET THE INDEX POSITION OF THE ELEMENTS
-console.log(text.indexOf("s"));
+// console.log(text.indexOf("s"));
 
-console.log(text.indexOf("script")); //output: 4 which index it is starting
+// console.log(text.indexOf("script")); //output: 4 which index it is starting
 
-console.log(text.indexOf("Java")); //output: 0 because J is 0 so it is strated in the 0 index position
+// console.log(text.indexOf("Java")); //output: 0 because J is 0 so it is strated in the 0 index position
+
+//TRIM
+
+let text = "         JavaScript";
+console.log(text.trim());
