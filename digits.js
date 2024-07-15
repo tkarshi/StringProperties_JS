9c0cfaddf856856f0b8596b7fdf179e8d152eb81
tkarshi <tkarshi2000@gmail.com>
2024-07-15 06:17:42 +0530

    split using for add space

--- digits.js
+++ digits.js
@@ -1,5 +1,11 @@
-function displayNumbers(number) {
-  console.log(String(number).padStart(3, "0"));
-}
+// function displayNumbers(number) {
+//   console.log(String(number).padStart(3, "0"));
+// }
 
-displayNumbers(5);
+// displayNumbers(5);
+
+let students = "student 1,student 2,student 3";
+
+let Student = students.split(",");
+
+console.log(Student);
