63b97cb422f15817f5112f77b9b056941368ef32
tkarshi <tkarshi2000@gmail.com>
2024-07-15 06:40:11 +0530

    create for loop and use the rest parameter

--- restParameter.js
+++ restParameter.js
@@ -1,4 +1,24 @@
-function addNumbers(n1, n2) {
-  console.log(n1 + n2);
+// function addNumbers(n1, n2) {
+//   console.log(n1 + n2);
+// }
+// addNumbers(10, 20);
+
+//DISPLAY AS ARRAY
+
+// function addNumber(...numbers) {
+//   console.log(numbers);
+// }
+
+// addNumber(10, 20, 30);
+
+//ADD NUMBERS
+
+function addNumbers(...numbers) {
+  let total = 0;
+  for (let n of numbers) {
+    total += n;
+  }
+  console.log(total);
 }
-addNumbers(10, 20);
+
+addNumbers(10, 30, 70);
