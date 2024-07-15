2425a2cc90624ca60d828c16b7e44b327a305722
tkarshi <tkarshi2000@gmail.com>
2024-07-15 05:51:02 +0530

    indexOf using for identify the index position of the elements and text

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let text = "Javascript";
 
-console.log(text);
+// console.log(text);
 
-console.log(text.slice(4)); //take the 4th index element
+// console.log(text.slice(4)); //take the 4th index element
 
-console.log(text.slice(0, 4)); //leave the 4th index element
+// console.log(text.slice(0, 4)); //leave the 4th index element
+
+//GET THE INDEX POSITION OF THE ELEMENTS
+console.log(text.indexOf("s"));
+
+console.log(text.indexOf("script")); //output: 4 which index it is starting
+
+console.log(text.indexOf("Java")); //output: 0 because J is 0 so it is strated in the 0 index position
